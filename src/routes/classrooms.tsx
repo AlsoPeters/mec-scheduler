5a3ceef7fbccadd8521aa7d68b07031a4295fa83
AlsoPeters <kyle.fire@gmail.com>
2025-02-23 16:37:39 +0900

    classroom component refinement

--- src/routes/classrooms.tsx
+++ src/routes/classrooms.tsx
@@ -7,27 +7,27 @@ export const Route = createFileRoute('/classrooms')({
 function ClassroomComponent() {
   return <>
     <div className='p-2'>Hello from Classrooms!</div>
-
-    <div id='classroomContainer' className='bg-amber-50'>
-
-      <div id='classroomNav' className='flex justify-between w-full px-2'>
+    {/* Container for the classroom component */}
+    <div id='classroomContainer' className='px-2'>
+      {/* Classroom NAV */}
+      <div id='classroomNav' className='flex justify-between w-full'>
 
         <div id='leftContainer' className='flex gap-2'>
-          <div id='roomNameContainer' className='border-2 px-2 text-center'>
+          <div id='roomNameContainer' className='bg-blue-200 border-2 border-b-0 px-2 text-center'>
             <div id='roomDropDown'>A</div>
             <p className='font-bold'>ROOM</p>
           </div>
-          <div id='teacher' className='border-2 px-2 flex items-center'>Kyle</div>
+          <div id='teacher' className='border-2 border-b-0 px-2 flex items-center bg-blue-200'>Kyle</div>
         </div>
-        
+
         <div id='rightContainer' className='flex gap-2'>
-          <div id='classroomLevel' className='font-bold border-2 flex items-center px-2'>E-1</div>
-          <div id='classroomTime' className='italic border-2 flex items-center px-2'>5:00</div>
+          <div id='classroomLevel' className='font-bold border-2 border-b-0 flex items-center px-2 bg-blue-200'>E-1</div>
+          <div id='classroomTime' className='italic border-2 flex items-center px-2 bg-blue-200 border-b-0'>5:00</div>
         </div>
 
       </div>
-
-      <div id='students' className='border-2 flex'>
+      {/* Student List */}
+      <div id='students' className='border-2 flex py-2 bg-blue-200'>
         <div id='groupOne' className='w-1/2 flex flex-col gap-2 p-2'>
           <div className='border-2 px-2'>Student 1</div>
           <div className='border-2 px-2'>Student 2</div>
@@ -41,6 +41,11 @@ function ClassroomComponent() {
           <div className='border-2 px-2'>Student 7</div>
         </div>
       </div>
+      {/* Teachers Notes area */}
+      <div id='teacherNotes' className='p-2 border-2 border-t-0 bg-blue-200'>
+        <p className='italic'>Teachers message here.</p>
+      </div>
+
     </div>
   </>
 }
